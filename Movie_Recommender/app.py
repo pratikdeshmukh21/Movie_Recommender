719ca72ea661d3c249e20c60d8a1066b43cfbdfc
PRATIK DESHMUKH <162615200+pratikdeshmukh21@users.noreply.github.com>
2025-03-07 12:26:43 +0530

    Update app.py
    change in ui

--- Movie_Recommender/app.py
+++ Movie_Recommender/app.py
@@ -1,61 +1,62 @@
 import pickle
 import streamlit as st
 import requests
+from PIL import Image
 
+
+# Function to fetch movie poster
 def fetch_poster(movie_id):
-    url = "https://api.themoviedb.org/3/movie/{}?api_key=9baab5cb452eb97da3d511fd2bed1d0e&language=en-US".format(movie_id)
-    data = requests.get(url)
-    data = data.json()
-    poster_path = data['poster_path']
-    full_path = "https://image.tmdb.org/t/p/w500/" + poster_path
-    return full_path
+    url = f"https://api.themoviedb.org/3/movie/{movie_id}?api_key=9baab5cb452eb97da3d511fd2bed1d0e&language=en-US"
+    data = requests.get(url).json()
+    poster_path = data.get('poster_path', '')
+    return f"https://image.tmdb.org/t/p/w500/{poster_path}" if poster_path else ""
 
+
+# Function to recommend movies
 def recommend(movie):
     index = movies[movies['title'] == movie].index[0]
     distances = sorted(list(enumerate(similarity[index])), reverse=True, key=lambda x: x[1])
     recommended_movie_names = []
     recommended_movie_posters = []
+
     for i in distances[1:6]:
-        # fetch the movie poster
         movie_id = movies.iloc[i[0]].movie_id
         recommended_movie_posters.append(fetch_poster(movie_id))
         recommended_movie_names.append(movies.iloc[i[0]].title)
-    print("Recommended Names:", recommended_movie_names)
-    print("Recommended Posters:", recommended_movie_posters)
-    return recommended_movie_names,recommended_movie_posters
+
+    return recommended_movie_names, recommended_movie_posters
 
 
-st.header('Movie Recommender System')
-movies = pickle.load(open('model/movie_list.pkl','rb'))
-similarity = pickle.load(open('model/similarity.pkl','rb'))
+# Load movie data and similarity matrix
+st.set_page_config(page_title="Movie Recommender", layout="wide")
+st.markdown("""
+    <style>
+        .big-font { font-size:30px !important; text-align: center; font-weight: bold; color: #FF4B4B; }
+        .sub-text { font-size:18px; text-align: center; color: #4B6CC1; }
+    </style>
+""", unsafe_allow_html=True)
 
-movie_list = movies['title'].values
-selected_movie = st.selectbox(
-    "Type or select a movie from the dropdown",
-    movie_list
-)
+movies = pickle.load(open('../MOVIE-RECOMMANDATION/modal/movie_list.pkl', 'rb'))
+similarity = pickle.load(open('../MOVIE-RECOMMANDATION/modal/similarity.pkl', 'rb'))
 
-if st.button('Show Recommendation'):
-    recommended_movie_names,recommended_movie_posters = recommend(selected_movie)
-    col1, col2, col3, col4, col5 = st.columns(5)
-    with col1:
-        st.text(recommended_movie_names[0])
-        st.image(recommended_movie_posters[0])
-    with col2:
-        st.text(recommended_movie_names[1])
-        st.image(recommended_movie_posters[1])
+st.markdown('<p class="big-font">🎬 Movie Recommender System 🍿</p>', unsafe_allow_html=True)
+st.markdown('<p class="sub-text">Find movies similar to your favorites!</p>', unsafe_allow_html=True)
 
-    with col3:
-        st.text(recommended_movie_names[2])
-        st.image(recommended_movie_posters[2])
-    with col4:
-        st.text(recommended_movie_names[3])
-        st.image(recommended_movie_posters[3])
-    with col5:
-        st.text(recommended_movie_names[4])
-        st.image(recommended_movie_posters[4])
+# Movie selection
+dropdown_col, _ = st.columns([3, 1])
+with dropdown_col:
+    selected_movie = st.selectbox("🎥 Select a movie from the dropdown", movies['title'].values)
 
+# Show recommendations
+if st.button('🔍 Show Recommendation'):
+    recommended_movie_names, recommended_movie_posters = recommend(selected_movie)
 
+    st.markdown("<hr>", unsafe_allow_html=True)
+    st.markdown('<p class="big-font">Recommended Movies 🎬</p>', unsafe_allow_html=True)
 
-
+    cols = st.columns(5)
+    for i, col in enumerate(cols):
+        with col:
+            st.write(f"**{recommended_movie_names[i]}**")  # Fixing the movie name display
+            st.image(recommended_movie_posters[i], width=200)
 
